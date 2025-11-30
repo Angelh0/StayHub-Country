6f02b7a8de02a9f0714ad06442f2b9de8d85c366
Angel <angelloopezz.hdez@gmail.com>
2025-11-30 22:18:00 +0000

    Primer commit de StayHub-Country

---
+++ src/main/java/stayhub_Country/stayhub_Country/StayhubCountryApplication.java
@@ -0,0 +1,13 @@
+package stayhub_Country.stayhub_Country;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayhubCountryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayhubCountryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/stayhub_Country/stayhub_Country/StayhubCountryApplicationTests.java
@@ -0,0 +1,13 @@
+package stayhub_Country.stayhub_Country;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayhubCountryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
